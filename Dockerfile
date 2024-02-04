b8e5067bfc32ecfd56430f7ee41bdf5923eb937c
Yosuke Santamaria <youman.318@gmail.com>
2024-02-04 17:30:07 +0900

    docker and sample code

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM python:3.11
+FROM python:3.10
 
 RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+
+WORKDIR tasks
+COPY requirements.txt /tasks/requirements.txt
+
+RUN pip install -r /tasks/requirements.txt
--- tasks/jsts/roberta_model.py
+++ tasks/jsts/roberta_model.py
@@ -1,14 +1,19 @@
+import torch
+
 # Download RoBERTa already finetuned for MNLI
 roberta = torch.hub.load('pytorch/fairseq', 'roberta.large.mnli')
 roberta.eval()  # disable dropout for evaluation
+
 
 with torch.no_grad():
     # Encode a pair of sentences and make a prediction
     tokens = roberta.encode('Roberta is a heavily optimized version of BERT.', 'Roberta is not very optimized.')
     prediction = roberta.predict('mnli', tokens).argmax().item()
     assert prediction == 0  # contradiction
+    print(prediction)
 
     # Encode another pair of sentences
     tokens = roberta.encode('Roberta is a heavily optimized version of BERT.', 'Roberta is based on BERT.')
     prediction = roberta.predict('mnli', tokens).argmax().item()
     assert prediction == 2  # entailment
+    print(prediction)
