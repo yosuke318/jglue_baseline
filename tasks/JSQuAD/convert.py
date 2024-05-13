e09e565246e977c641fde87fcb09ab7a64cd0f89
yosuke_maruyama <yosuke.maruyama@hightec.co.jp>
2024-05-13 14:09:55 +0900

    jsonを以下の記事で読み込み可能なデータ形式へ変換 (https://huggingface.co/docs/transformers/v4.19.2/en/tasks/question_answering)

--- tasks/JSQuAD/convert.py
+++ tasks/JSQuAD/convert.py
@@ -16,13 +16,20 @@ def convert_json_format(data):
                 converted_data.append(converted_item)
     return converted_data
 
+
 def load_json_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 
+def save_json_file(data, file_path):
+    with open(file_path, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
 if __name__ == "__main__":
     data = load_json_file('./training.json')
     converted_data = convert_json_format(data['train'])
     print(converted_data)
+    save_json_file(converted_data, "./new_training.json")
